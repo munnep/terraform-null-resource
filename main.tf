57e410ca435a0a837d46e1a4cfc0db032a546e38
munnep <patrick.munne@hashicorp.com>
2024-01-18 14:36:07 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,4 +2,14 @@ resource "null_resource" "test" {}
 
 variable "variable_string" {
 default = "this is the default_value"
+description = "This is just to see if we have a description"
+}
+
+
+variable "variable_map" { 
+type = map(string)
+  default = {
+    Name = "example"
+  }
+description = "This is just to see if we have a description"
 }
