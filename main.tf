268a871e1888bf7c6756de43ba947508751ae990
munnep <patrick.munne@hashicorp.com>
2024-01-18 14:46:00 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -33,3 +33,36 @@ variable "resource_groups2" {
     description = "This is just to see if we have a description"
     default = {}
 }
+
+variable "resource_groups3" {
+    type = map(object({                     
+       resource_group_name = string      
+        location = string                
+        tags = map(string)         
+    }))
+    description = "This is just to see if we have a description"
+    default = {}
+}
+
+
+variable "resource_groups4" {
+  type = object({
+    a = string                # a required attribute
+    b = optional(string)      # an optional attribute
+    c = optional(number, 127) # an optional attribute with default value
+  })
+    description = "This is just to see if we have a description"
+}
+
+
+variable "buckets" {
+  type = list(object({
+    name    = string
+    enabled = optional(bool, true)
+    website = optional(object({
+      index_document = optional(string, "index.html")
+      error_document = optional(string, "error.html")
+      routing_rules  = optional(string)
+    }), {})
+  }))
+}
