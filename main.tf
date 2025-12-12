c1cab03ce8ccb3fb78b120bc3d373b983f63cc01
munnep <patrick.munne@hashicorp.com>
2025-12-12 13:29:14 +0100

    version 11.0.0

--- main.tf
+++ main.tf
@@ -1,19 +1,7 @@
 resource "terraform_data" "name" {
-  input = "test7"
+  input = "11.0.0"
 }
 
-output "example" {
-  value = terraform_data.name.output
-}
-
-data "external" "slow_delay" {
-  program = ["bash", "-c", <<EOT
-    sleep 3
-    echo '{ "result": "done" }'
-EOT
-  ]
-}
-
-output "delay_result" {
-  value = data.external.slow_delay2.result
+output "version" {
+  value = terraform_data.name.input
 }
