969faa011aeeac8abb2820b4716ce66fac94b60c
munnep <patrick.munne@hashicorp.com>
2025-12-12 11:31:19 +0100

    test

--- main.tf
+++ main.tf
@@ -1,24 +1,7 @@
-resource "null_resource" "test" {}
-
-
-resource "null_resource" "test2" {}
-
-resource "null_resource" "test3" {}
-
-resource "null_resource" "test4" {}
-
-resource "null_resource" "test5" {}
-
-output "test_output" {
-value = "please break"
+resource "terraform_data" "name" {
+  input = "test1"
 }
 
-
-output "test_output2" {
-value = "please break. pretty please"
-}
-
-
-output "test_output3" {
-value = "please break. Come on"
+output "example" {
+  value = terraform_data.name.output
 }
