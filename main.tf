d4ad8859d6322fd072480fd3c15aff562abd586f
munnep <patrick.munne@hashicorp.com>
2025-12-17 09:54:07 +0100

    Update input version and add failure provisioner
    Updated input version to 15.0.0 and added a new resource to fail on apply.

--- main.tf
+++ main.tf
@@ -1,7 +1,13 @@
 resource "terraform_data" "name" {
-  input = "14.0.0"
+  input = "15.0.0"
 }
 
 output "version" {
   value = terraform_data.name.input
+}
+
+resource "terraform_data" "fail_on_apply_local_exec" {
+  provisioner "local-exec" {
+    command = "exit 1"  # This will fail during apply
+  }
 }
