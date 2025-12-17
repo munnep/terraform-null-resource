ef1f6167ae22e0b6095f2cdc3e74cc03752f4cf6
munnep <patrick.munne@hashicorp.com>
2025-12-17 10:43:57 +0100

    Update Terraform input versions to 16.0.0test

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 resource "terraform_data" "name" {
-  input = "15.0.0"
+  input = "16.0.0"
 }
 
 output "version" {
-  value = terraform_data.name.input
+  value = terraform_data.fail_on_apply_local_exec.input
 }
 
 resource "terraform_data" "fail_on_apply_local_exec" {
+  input = "16.0.0"
   provisioner "local-exec" {
     command = "exit 1"  # This will fail during apply
   }
