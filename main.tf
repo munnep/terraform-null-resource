943c2eb35d9b9bfcc29cb3a34b6eed5427f9045a
munnep <patrick.munne@hashicorp.com>
2026-02-20 15:49:40 +0100

    Remove fail_on_apply_local_exec resource
    Removed the fail_on_apply_local_exec resource from main.tf.

--- main.tf
+++ main.tf
@@ -6,9 +6,3 @@ output "version" {
   value = terraform_data.fail_on_apply_local_exec.input
 }
 
-resource "terraform_data" "fail_on_apply_local_exec" {
-  input = "16.0.0"
-  provisioner "local-exec" {
-    command = "exit 1"  # This will fail during apply
-  }
-}
